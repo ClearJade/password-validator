6dd25b9873ef87cc878288f22611fe4562a07739
ClearJade <clearjade96@hotmail.com>
2017-05-29 00:20:27 -0300

    Assignment 2 part 3 - UI added

--- app/src/androidTest/java/com/samantha/password_validator/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/samantha/password_validator/ExampleInstrumentedTest.java
@@ -2,11 +2,20 @@ package com.samantha.password_validator;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.test.rule.ActivityTestRule;
 
+
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.*;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.*;
 import static org.junit.Assert.*;
 
 /**
@@ -15,12 +24,58 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    private String strongText;
+    private String weakText;
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.samantha.password_validator", appContext.getPackageName());
+    }
+
+    @Before
+    public void initString()
+    {
+        strongText = "Password is strong";
+
+        weakText = "Password is not strong enough";
+    }
+
+    @Test
+    public void testStrong()
+    {
+        //Test strong test
+        // Type text and then press the button.
+        onView(withId(R.id.editText))
+                .perform(typeText("TesTinG123"), closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+
+        //Does test match
+        onView(withId(R.id.textView)).check(matches(withText(strongText)));
+
+    }
+
+
+    @Test
+    public void testWeak()
+    {
+        //Test strong test
+        // Type text and then press the button.
+        onView(withId(R.id.editText))
+                .perform(typeText("test"), closeSoftKeyboard());
+        onView(withId(R.id.button)).perform(click());
+
+        //Does test match
+        onView(withId(R.id.textView)).check(matches(withText(weakText)));
+
     }
 }
--- app/src/main/java/com/samantha/password_validator/MainActivity.java
+++ app/src/main/java/com/samantha/password_validator/MainActivity.java
@@ -2,6 +2,9 @@ package com.samantha.password_validator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /** Called when the user taps the Validate button */
+    public void evaluatePasswordStrength(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+
+        ValidatePassword password = new ValidatePassword(editText.getText().toString());
+
+
+        String message = getMessage(password);
+        TextView textToReplace = (TextView) findViewById(R.id.textView);
+        textToReplace.setText(message);
+    }
+
+    private String getMessage(ValidatePassword password){
+        String message = "";
+        int strength = password.getPasswordStrength();
+
+        if(strength == 5){
+            message = "Password is strong";
+        }
+        else{
+            message = "Password is not strong enough";
+        }
+
+        return message;
+    }
+
+
 }
